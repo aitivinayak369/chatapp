6b7e9cf8e6a5b3eec36eeaa81e9fb327ca9ead29
aitivinayak369 <aitivinayak369@gmail.com>
2017-12-19 21:09:12 +0530

    2chat

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 var express =require('express');
+var http=require('http');
+var socketIO=require('socket.io');
 var app=express();
+var server=http.createServer(app);
+var IO=socketIO(server);
 const path=require('path');
 var pathJoin =path.join('__dirname','../public');
 var port=process.env.PORT||3000;
 app.use(express.static(pathJoin));
-app.listen(port,()=>{
+IO.on('connection',()=>{
+    console.log('connected to server');
+})
+server.listen(port,()=>{
     console.log(`Listening to port:${port}`)
 })
